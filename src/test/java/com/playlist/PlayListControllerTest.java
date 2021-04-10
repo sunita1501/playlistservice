17451b4d83b8497006eb9c322789ddc26d43665b
maanick <maanickavasagam@gmail.com>
2021-04-10 15:50:54 -0600

    updated rest docs details

--- src/test/java/com/playlist/PlayListControllerTest.java
+++ src/test/java/com/playlist/PlayListControllerTest.java
@@ -3,11 +3,16 @@ package com.playlist;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -17,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @Transactional
 public class PlayListControllerTest {
 
@@ -46,13 +52,17 @@ public class PlayListControllerTest {
                         .content(objectMapper.writeValueAsString(input))
                         .contentType(MediaType.APPLICATION_JSON)
         )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("playlist"));;
 
         mockMvc.perform(get("/playlist"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(1))
                 .andExpect(jsonPath("[0].name").value("PlayList1"))
-                .andExpect(jsonPath("[0].songs").value(""));
+                .andExpect(jsonPath("[0].songs").value(""))
+                .andDo(document("playlist", responseFields(
+                        fieldWithPath("[0].name").description("PlayList1"),
+                        fieldWithPath("[0].songs").description(""))));
     }
 
 
