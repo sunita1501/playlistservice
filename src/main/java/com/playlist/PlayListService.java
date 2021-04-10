86f92d9abde1ecfe16a74696464c42c254ed689e
maanick <maanickavasagam@gmail.com>
2021-04-10 10:58:23 -0600

    Red Test for array list implementation

--- src/main/java/com/playlist/PlayListService.java
+++ src/main/java/com/playlist/PlayListService.java
@@ -1,16 +1,20 @@
 package com.playlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayListService {
 
-    String play1;
+    //String play1;
+    List<String> play1 = new ArrayList<>();
 
-    public Object getPlayList() {
+    /*public Object getPlayList() {
 
         return null;
 
-    }
+    }*/
 
-    public String getPlayList1() {
+    public List<String> getPlayList1() {
 
 
         return play1;
--- src/test/java/com/playlist/PlayListServiceTest.java
+++ src/test/java/com/playlist/PlayListServiceTest.java
@@ -3,9 +3,11 @@ package com.playlist;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+//import static org.junit.jupiter.api.AssertTrue.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 public class PlayListServiceTest {
 
     PlayListService service;
@@ -19,16 +21,18 @@ public class PlayListServiceTest {
     @Test
     public void gPlayListDBIsEmptyTest() {
 
-        assertNull(service.getPlayList());
+        assertNull(service.getPlayList1());
+        //assertTrue(service.getPlayList().);
     }
 
 
     @Test
     public void gPlayListDBReturnsPlayListTest() {
 
-        service.play1 = "Playlist1";
+        //service.play1 = "Playlist1";
+        service.play1.add("Playlist1");
 
-        assertEquals(service.getPlayList1(), "Playlist1");
+        assertEquals(service.getPlayList1().get(0), "Playlist1");
     }
 
 }
