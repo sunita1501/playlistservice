b0e06c8144195def6deffcb4d8f346f4fc498cd0
maanick <maanickavasagam@gmail.com>
2021-04-10 12:41:07 -0600

    Red Test for Acceptance Criteria 1 to create play list

--- src/test/java/com/playlist/PlayListControllerTest.java
+++ src/test/java/com/playlist/PlayListControllerTest.java
@@ -1,12 +1,15 @@
 package com.playlist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,11 +23,34 @@ public class PlayListControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /*When a playlist is created with a name
+    Then a confirmation is returned that it was successful.
+    And the playlist is empty.*/
     @Test
     void fetchEmptyList() throws Exception {
-
+        //VisitorDTO input = new VisitorDTO("Zackry","Good");
         mockMvc.perform(get("/playlist"))
                 .andExpect(status().isOk());
 
+    }
+
+    @Test
+    void createPlayListTest() throws Exception {
+        PlayListDTO input = new PlayListDTO("PlayList1","");
+        mockMvc.perform(
+                post("/playlist")
+                        .content(objectMapper.writeValueAsString(input))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/playlist"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(1))
+                .andExpect(jsonPath("[0].name").value("PlayList1"));
+                //.andExpect(jsonPath("[0]").value("Good"));
     }
 }
