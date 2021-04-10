0a0451163cb75dab6fc3c06308bad5945b88b96b
sunita1501 <sunita.kumari@cognizant.com>
2021-04-10 11:31:23 -0400

    Green on Empty PlayList

---
+++ src/main/java/com/playlist/PlayListDB.java
@@ -0,0 +1,11 @@
+package com.playlist;
+
+public class PlayListDB {
+
+
+
+    public Object getPlayList() {
+
+        return null;
+    }
+}
--- src/test/java/com/playlist/PlayListIT.java
+++ src/test/java/com/playlist/PlayListIT.java
@@ -2,13 +2,15 @@ package com.playlist;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class PlayListIT {
 
     @Test
     public void gPlayListDBIsEmptyTest() {
         PlayListDB playlist = new PlayListDB();
 
-        assertNull(playlist.getplayList());
+        assertNull(playlist.getPlayList());
     }
 
 }
